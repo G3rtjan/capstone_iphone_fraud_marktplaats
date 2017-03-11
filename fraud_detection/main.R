9864e8facb33ddcd9d41ec766470202d46e0836a
Tim Vink <vinktim@gmail.com>
2017-03-11 13:52:36 +0100

    image hashing calculation

--- fraud_detection/main.R
+++ fraud_detection/main.R
@@ -3,7 +3,7 @@
 #### Find fraudulent marktplaats iPhone ads ####
 ################################################
 
-# We have gathered iPhone ads (including images) from ±Dec-16 - ±Feb17 using a scheduled web scraper
+# We have gathered iPhone ads (including images) from ??Dec-16 - ??Feb17 using a scheduled web scraper
 # The results are stored in a google bucket
 # Download to /data (which is .gitignored) before proceding
 
@@ -26,14 +26,24 @@ purrr::walk(list.files("functions", full.names = T), source)
 #### Aggregate data #### 
 
 # image hash map
-image_hash_table <- list.files("../data/mpimages", full.names = T)[1:1000] %>% 
+i <- 0 # for printing
+image_hash_table <- list.files("../data/mpimages", full.names = T) %>% 
   purrr::map_df(hash_image)
 writeRDS(image_hash_table, "data/image_hash_table.RData")
 
 # ads data 
 
 # merchant data
+# Get all adds data
+all_adds <- readRDS("../data/full_mp_data.RData")
 
+# Get all unique merchants with n open adds (or indication 'Removed')
+all_merchants <- all_adds %>% 
+  dplyr::select(cp_id,counterparty) %>% 
+  dplyr::distinct(.keep_all = T) %>% 
+  dplyr::arrange(cp_id,counterparty) %>% 
+  dplyr::rowwise() %>% 
+  dplyr::mutate(n_ads = get_n_current_advs_of_merchant(cp_id))
 
 
 
